9d22eed4b14727e0892ed5434239272f7b73c4d4
Huwei <1078786004@qq.com>
2019-10-07 08:44:55 +0800

    shop项目初始化

---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from '@/components/login'
+import Home from '@/components/home'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/login',
+      component: Login
+    },
+    {
+      path: '/home', component: Home
+    },
+    {
+      path: '/', redirect: '/home'
+    }
+  ]
+})
