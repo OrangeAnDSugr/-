28623f976a1526efc9b2fd0ca89145beed8e9c21
Huwei <1078786004@qq.com>
2019-10-11 11:16:23 +0800

    商品分类完成

--- src/components/category/Category.vue
+++ src/components/category/Category.vue
@@ -1,8 +1,16 @@
 <template>
   <div>
+    <!-- 面包屑导航头部 -->
+    <el-breadcrumb separator="/" class="goodNav">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>商品分类</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-button type="success" plain @click="showAddCategoryDialog">添加分类</el-button>
+    <!-- 数据表格 -->
     <el-table :data="goodsCateTable" stripe style="width: 100%" v-loading="loading">
-      <el-table-column prop="cat_name" label="分类名称" width="280">
-      </el-table-column>
+      <!-- 树形控件结构 -->
+      <el-table-tree-column tree-key="cat_id" level-key="cat_level" parent-key="cat_pid" child-key="children" :indent-size="30" file-icon="icon icon-file" folder-icon="el-icon-folder" prop="cat_name" label="分类名称" width="280"></el-table-tree-column>
       <el-table-column prop="cat_deleted" label="是否有效" width="180">
         <template slot-scope="scope">
           <el-row v-if="!scope.row.cat_deleted">无效</el-row>
@@ -18,8 +26,33 @@
         </template>
       </el-table-column>
     </el-table>
+    <!-- 分页模块 -->
+    <el-pagination background layout="prev, pager, next" :total="total" :page-size="pageSize" @current-change="goAnotherPage">
+    </el-pagination>
+    <!-- 添加分类对话框 -->
+    <el-dialog title="收货地址" :visible.sync="addDialogVisible">
+      <el-form :model="addCategoryForm">
+        <el-form-item label="分类名称" :label-width="formLabelWidth">
+          <el-input v-model="addCategoryForm.cat_name" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="父级名称" :label-width="formLabelWidth">
+          <el-cascader :options="addCategoryForm.categoryList" :props="addCategoryForm.options" :v-model="addCategoryForm.categoriesSelectedOptions"
+          clearable></el-cascader>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="addDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click.prevent="addCategory">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
 <script src="./script.js"></script>
-<style></style>
+<style lang="less">
+.goodNav {
+  margin-bottom: 10px;
+  padding: 15px;
+  background-color: #d4dae0;
+}
+</style>
--- src/components/category/script.js
+++ src/components/category/script.js
@@ -5,11 +5,27 @@ export default {
       goodsCateTable: [],
       pageSize: 10,
       curPage: 1,
-      loading: false
+      loading: false,
+      total: -1,
+      addDialogVisible: false,
+      formLabelWidth: '100px',
+      addCategoryForm: {
+        cat_name: '',
+        cat_pid: -1,
+        cat_level: -1,
+        categoryList: [],
+        categoriesSelectedOptions: [],
+        options: {
+          checkStrictly: true,
+          value: 'cat_id',
+          label: 'cat_name'
+        }
+      }
     }
   },
   created() {
     this.getCategoryList()
+    this.getCategoryAddList()
   },
   methods: {
     async getCategoryList(curPage = 1) {
@@ -17,14 +33,56 @@ export default {
       const res = await this.$http.get('/categories', {
         params: {
           pagenum: curPage,
-          pageSize: this.pageSize
+          pagesize: this.pageSize
         }
       })
       console.log(res)
       const { data, meta } = res.data
       if (meta.status === 200) {
-        this.goodsCateTable = data
+        this.goodsCateTable = data.result
+        this.total = data.total
         this.loading = false
+      }
+    },
+    // 去往当前页
+    goAnotherPage(curPage) {
+      this.curPage = curPage
+      this.getCategoryList(curPage)
+    },
+    showAddCategoryDialog() {
+      this.addDialogVisible = true
+    },
+    async getCategoryAddList() {
+      const res = await this.$http.get('/categories', {
+        params: {
+          type: 2
+        }
+      })
+      console.log(res)
+      const { data, meta } = res.data
+      if (meta.status === 200) {
+        this.addCategoryForm.categoryList = data
+      }
+    },
+    // 添加分类
+    async addCategory() {
+      const {
+        cat_name: catName,
+        categoriesSelectedOptions
+      } = this.addCategoryForm
+      const catPid =
+        categoriesSelectedOptions[categoriesSelectedOptions.length - 1]
+      const catLevel = categoriesSelectedOptions.length
+      const res = await this.$http.post('/categories', {
+        cat_pid: catPid,
+        cat_name: catName,
+        cat_level: catLevel
+      })
+      console.log(res)
+      const { meta, data } = res.data
+      if (meta.status === 201) {
+        this.getCategoryList(this.curPage)
+        this.addDialogVisible = false
       }
     }
   },
